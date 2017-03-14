ab777131311199a6d5fd8001ae1c66f7ee82132b
nefto <nefto_2000@abv.bg>
2017-03-14 05:40:18 +0200

    last

---
+++ 2_FormattingHomework/Events/StartUp.cs
@@ -0,0 +1,24 @@
+﻿namespace Events
+{
+    using System;
+    using System.Text;
+    using Contracts;
+    using Core;
+    using Models;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var reader = new ConsoleReader();
+            bool commandSuccess = Engine.ExecuteNextCommand(reader);
+
+            while (commandSuccess)
+            {
+                commandSuccess = Engine.ExecuteNextCommand(reader);
+            }
+
+            Console.WriteLine(Messages.Output);
+        }
+    }
+}
